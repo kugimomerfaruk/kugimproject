ebf2bde3da82b46b06c6124f6c1c1c18c4a1a7f8
ST6 <st6@ST6-iMac.local>
2017-09-28 14:05:09 +0300

    Clear Button Added

--- KugimOmerFaruk/ViewController.swift
+++ KugimOmerFaruk/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBAction func addNameButton() {
         
     }
+    @IBAction func clearButton() {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
