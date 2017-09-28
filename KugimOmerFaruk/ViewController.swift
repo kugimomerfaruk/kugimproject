a5f9d97226a8982f7dc9ef74dc2174a72f5a20d0
ST6 <st6@ST6-iMac.local>
2017-09-28 13:20:47 +0300

    Initial Commit

---
+++ KugimOmerFaruk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KugimOmerFaruk
+//
+//  Created by ST6 on 28.09.2017.
+//  Copyright © 2017 kugim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
