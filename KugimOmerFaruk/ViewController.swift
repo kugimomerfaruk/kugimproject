5c01db8c9bcc801da205d6fbfd7f901d1279bf5a
ST6 <st6@ST6-iMac.local>
2017-09-28 13:41:16 +0300

    AddNameButton Added

--- KugimOmerFaruk/ViewController.swift
+++ KugimOmerFaruk/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nameEnterTextBox: UILabel!
+    @IBAction func addNameButton() {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
