c9791587e6be3b5f0064f71041e88283f3fe028f
ST6 <st6@ST6-iMac.local>
2017-09-28 14:17:37 +0300

    Character Alert Button Added and Hidden

--- KugimOmerFaruk/ViewController.swift
+++ KugimOmerFaruk/ViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameEnterTextBox: UITextField!
     @IBOutlet weak var nameList: UILabel!
-    @IBOutlet weak var nameEnterTextBox: UILabel!
+   
+    @IBOutlet weak var alertForCharacterControl: UILabel!
+    
+    
+    
+    
     @IBAction func addNameButton() {
+        
         
     }
     @IBAction func clearButton() {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    alertForCharacterControl.isHidden = true
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
