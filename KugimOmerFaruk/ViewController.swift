bc06e208312dcfd0151c8fe1554120fc3e02b3d4
ST6 <st6@ST6-iMac.local>
2017-09-28 15:40:13 +0300

    Add names input clear action

--- KugimOmerFaruk/ViewController.swift
+++ KugimOmerFaruk/ViewController.swift
@@ -15,40 +15,38 @@ class ViewController: UIViewController {
     var names : String = ""
     var namesArray = [String] ()
     
-    
-    
     @IBOutlet weak var nameEnterTextBox: UITextField!
     @IBOutlet weak var nameList: UILabel!
     @IBOutlet weak var alertForCharacterControl: UILabel!
     
     @IBAction func addNameButton() {
         
-            if (nameEnterTextBox.text?.characters.count)! >= 5 {
-                names = nameEnterTextBox.text!
-                namesArray.append(names)
-                print(namesArray)
-                names = ""
-                
-                for i in namesArray {
-                    
+        if (nameEnterTextBox.text?.characters.count)! >= 5 {
+            names = nameEnterTextBox.text!
+            namesArray.append(names)
+            print(namesArray)
+            names = ""
+            
+            for i in namesArray {
                 print(i)
                 names += "\n" + i
-
-                }
-                nameList.text = names
-                
-            } else {
-                alertForCharacterControl.isHidden = false
             }
+            nameEnterTextBox.text = ""
+            
+            nameList.text = names
+            
+        } else {
+            alertForCharacterControl.isHidden = false
+        }
     }
     
-   
+    
     @IBAction func clearButton() {
         
         if namesArray.count != 0 {
             
-        namesArray.removeLast()
-        print(namesArray)
+            namesArray.removeLast()
+            print(namesArray)
             names = ""
             
             for i in namesArray {
@@ -59,36 +57,25 @@ class ViewController: UIViewController {
             }
             nameList.text = names
             
-            
         } else {
-        alertForCharacterControl.isHidden = false
-        alertForCharacterControl.text = "Silinecek bir veri bulunamadı"
+            alertForCharacterControl.isHidden = false
+            alertForCharacterControl.text = "Silinecek bir veri bulunamadı"
         }
-        
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-    alertForCharacterControl.isHidden = true
-        
-        
-        
-        
-        
-        
-        
+        alertForCharacterControl.isHidden = true
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
